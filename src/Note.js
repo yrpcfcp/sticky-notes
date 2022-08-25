d65ac2b6b0de2a6609f3eff13f3a76874a03b2ca
yrpcfcp <carlosfparana@gmail.com>
2022-08-24 22:13:18 -0300

    .

--- src/Note.js
+++ src/Note.js
@@ -1,20 +1,39 @@
 import React from "react";
 
-const Note = (props) => (
-  <li className="note">
-    <input
-      type="text"
-      placeholder="Title"
-      className="note__title"
-      value={props.note.title}
-    />
-    <textarea
-      placeholder="Description..."
-      className="note__description"
-      value={props.note.description}
-    />
-    <span className="note__delete">X</span>
-  </li>
-);
+
+const Note = (props) => {
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+  const clickDelete = () => props.removeNote(props.note.id);
+
+  return (
+    <li className="note">
+      <input
+        type="text"
+        value={props.note.title}
+        onChange={updateTitle}
+        placeholder="Title"
+        className="note__title"
+      />
+      <textarea
+        value={props.note.description}
+        onChange={updateDescription}
+        placeholder="Description..."
+        className="note__description"
+      />
+      <span onClick={clickDelete} className="note__delete">
+        X
+      </span>
+    </li>
+  );
+};
 
 export default Note;
--- src/NotesList.js
+++ src/NotesList.js
@@ -4,7 +4,16 @@ import Note from "./Note.js";
 const NotesList = (props) => {
   const keepSearchMatches = (note) => note.doesMatchSearch;
   const searchMatches = props.notes.filter(keepSearchMatches);
-  const renderNote = (note) => <Note note={note} key={note.id} />;
+
+  const renderNote = (note) => (
+    <Note
+      note={note}
+      key={note.id}
+      onType={props.onType}
+      removeNote={props.removeNote}
+    />
+  );
+
   const noteElements = searchMatches.map(renderNote);
   return <ul className="notes-list">{noteElements}</ul>;
 };
